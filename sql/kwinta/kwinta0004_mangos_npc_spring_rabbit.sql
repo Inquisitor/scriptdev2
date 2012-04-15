ddd181c9e59aee051dd3811384d942b29fd7eebb
Inquisitor <ssd_@ukr.net>
2012-04-15 22:50:08 +0300

    + typo

--- sql/kwinta/kwinta0004_mangos_npc_spring_rabbit.sql
+++ sql/kwinta/kwinta0004_mangos_npc_spring_rabbit.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` set `ScriptName` = 'npc_spring_rabbit' WHERE `entry` IN (37291);
+UPDATE `creature_template` set `ScriptName` = 'npc_spring_rabbit' WHERE `entry` IN (32791);
